e9986c43449fc346c7df1fac70aed0e43145d78c
Anshad Habib <eaglefitnessgroup@gmail.com>
2026-08-13 13:46:51 +0400

    feat: prepare EagleFlow for web deployment

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_web_plugins/url_strategy.dart';
 import 'app/app.dart';
 
 import 'core/di/service_locator.dart';
 
 void main() {
+  usePathUrlStrategy();
   WidgetsFlutterBinding.ensureInitialized();
   ServiceLocator().init();
   runApp(const EagleFlowApp());
